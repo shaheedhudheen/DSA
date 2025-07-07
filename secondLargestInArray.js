2afe2d0c193d231fa5b209cdf7c7c2f6b9e5ede8
Ahammed Shaheedhudheen <44499031+shaheedhudheen@users.noreply.github.com>
2025-07-07 23:49:18 +0530

    updated

---
+++ secondLargestInArray.js
@@ -0,0 +1,11 @@
+let arr = [12, 35, 1, 10, 34, 1];
+
+function findLargest(arr) {
+  let firstLargest = -Infinity;
+  let secondLargest = -Infinity;
+
+  for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+    
+  }
+}
